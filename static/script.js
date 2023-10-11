2eca6f076d02c920db84224d765d703f896475a7
ravindra kumar saini <raveensbsaini@gmail.com>
2023-10-11 18:26:55 +0530

    test 10 add static folder

---
+++ static/script.js
@@ -0,0 +1,7 @@
+addEventListener('DOMContentLoaded',function(){
+    console.log("stage1")
+    a = document.querySelector('.button')
+    a.addEventListener('click', function(){
+        alert("welcome to my webiste . i will repsond your message shortly")
+    })
+})
