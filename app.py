a9483d82f1a6a3939eec6949059cdb2ef03440fa
ravindra kumar saini <raveensbsaini@gmail.com>
2023-10-16 01:07:48 +0530

    take a session to debug code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request,url_for
 # import sqlite3
 import resend
 import os
+import psycopg2
 # postgres://raveensbsaini:U7N8FmrFrXNvL5P7brCkYJZA332hhTQp@dpg-ckiqu8b6fquc739in3fg-a.singapore-postgres.render.com/ravindrakumarsaini
 app = Flask(__name__)
 
@@ -15,23 +16,42 @@ def index():
      elif request.method == "POST":
          name =request.form.get("pname") 
          number = request.form.get("pnumber")
+         print(type(number),number)
+         number = int(number)
+         print(type(number),number)
+
 
          email = request.form.get("pemail")
          message = request.form.get("pmessage")
-         number = int(number)
-    #     database = sqlite3.connect("database.db")
-    #     cur = database.cursor()
-    #     cur.execute("INSERT INTO person (name, email, phone, message) VALUES (?, ?, ?, ?)",
-    #         (name, email, number, message))
-    #     database.commit()
-    #     cur.execute("select * from person;")
-    #     row = cur.fetchall()
-    #     for r in row:
-    #         print(r)
-    #     database.close()
+        
 
-    # #    re_3piwLq4L_PJaLnrFtgbs5YgTKK3eu7XT3
-         resend.api_key = os.environ.get("envapikey1")
+
+
+         conn = psycopg2.connect(
+         database="ravindrakumarsaini",
+         user="raveensbsaini",
+         password="U7N8FmrFrXNvL5P7brCkYJZA332hhTQp",
+         host="dpg-ckiqu8b6fquc739in3fg-a.singapore-postgres.render.com",
+         port=5432
+         )
+        
+
+         cur = conn.cursor()
+         cur.execute("select count(*) from person;")
+         rows = cur.fetchall()
+         id = rows[0][0] +1
+         query = "INSERT INTO person (id, name, email, message, number) VALUES ('{id}','{name}','{email}','{message}',{number})".format(id=id,name=name,email=email,message=message,number = number)
+         print(query)
+         cur.execute(query)
+
+         cur.close()
+         conn.commit() 
+
+
+
+
+
+         resend.api_key = "re_BzNodrVF_9yDdY1Hr3bcVPKCPMnmZ94Bx"
 
          params = {
              "from": "Acme <onboarding@resend.dev>",
